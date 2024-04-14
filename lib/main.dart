0d2ac93b45937dd90e8ccf68d536c7b848c37814
ChaitanyaHazarika <chaitanyahazarika9@gmail.com>
2024-04-14 11:59:55 +0530

    hello

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,12 @@ import 'package:first_app/gradient_container.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+final int x = 10;
+  final int y = 10;
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: GradientContainer(colors:[
+        body: GradientContainer(colors: [
           Color.fromARGB(255, 26, 2, 80),
           Color.fromARGB(255, 45, 7, 98)
         ]),
