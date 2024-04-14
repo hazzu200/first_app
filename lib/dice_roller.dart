1de7acaed0a17298ebe8c07a5cc0da77f46f3c81
ChaitanyaHazarika <chaitanyahazarika9@gmail.com>
2024-04-14 11:56:48 +0530

    first commit

---
+++ lib/dice_roller.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DiceRoller extends StatefulWidget {
+  const DiceRoller({super.key});
+  @override
+  State<DiceRoller> createState() {
+    return _DiceRollerState();
+  }
+}
+
+class _DiceRollerState extends State<DiceRoller> {
+  var activeDiceImage = 'assets/images/dice-2.png';
+
+  void rollDice() {
+    var diceRoll = Random().nextInt(6) + 1;
+    setState(() {
+      activeDiceImage = 'assets/images/dice-$diceRoll.png';
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Image.asset(
+          activeDiceImage,
+          width: 200,
+        ),
+        TextButton(
+          style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            padding: const EdgeInsets.all(20),
+            textStyle: const TextStyle(
+              fontSize: 28,
+            ),
+          ),
+          onPressed: rollDice,
+          child: const Text('Roll Dice!'),
+        )
+      ],
+    );
+  }
+}
